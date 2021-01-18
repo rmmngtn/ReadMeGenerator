13193345abddb77dae403a8b61fc730bed8f7d5a
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-17 17:01:14 -0800

    init function created, and mini project code pasted for reference

--- Develop/index.js
+++ Develop/index.js
@@ -12,7 +12,63 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers, data) => {   
+        console.log("Creating ReadME file");
+        fs.writeFileSync("ReadMe.md", inquirerAnswers, data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
 
 // Function call to initialize app
 init();
+
+
+
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your Github username',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is your location?',
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: 'What hobbies do you have?',
+            name: 'hobbies',
+        },
+        {
+            type: 'input',
+            message: 'What is your LinkedIn URL?',
+            name: 'linkedin',
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'github',
+        }
+
+    ]);
+
+
+const generateMarkdown = (answers) =>
+
+
+
+promptUser()
+    .then((answers) => writeFileAsync('README.md', generateMarkdown(answers)))
+        .then(() => console.log("Successfully wrote to README.md"))
+            .catch((err) => console.error(err));
+
+
+    
