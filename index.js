75c080826b73d8c2e2a64378e3d4641f634d9d53
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-23 15:53:15 -0800

    fixed functions and code flow on index.js

--- index.js
+++ index.js
@@ -8,93 +8,72 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Write a brief description of your project.',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'What installation steps are required for this project?',
-            name: 'install',
-        },
-        {
-            type: 'input',
-            message: 'How can others use this project?',
-            name: 'usage',
-        },
-        {
-            type: 'checkbox',
-            message: 'What license does this app have?',
-            name: 'license',
-            choices: [
-                "MIT", 
-                "ISC", 
-                "Apache 2.0",  
-                "BSD", 
-                "None",
-            ]
-        },
-        {
-            type: 'input',
-            message: 'Who can contribute to this project?',
-            name: 'contribute',
-        },
-        {
-            type: 'input',
-            message: 'What tests does this project use?',
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: 'What is your Github URL?',
-            name: 'github',
-        },
-        {
-            type: 'input',
-            message: 'What is your email?',
-            name: 'email',
+function init() {
+    function questions() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is your project title?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Write a brief description of your project.',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'What installation steps are required for this project?',
+                name: 'install',
+            },
+            {
+                type: 'input',
+                message: 'How can others use this project?',
+                name: 'usage',
+            },
+            {
+                type: 'checkbox',
+                message: 'What license does this app have?',
+                name: 'license',
+                choices: [
+                    "MIT",
+                    "ISC",
+                    "Apache 2.0",
+                    "BSD",
+                    "None",
+                ]
+            },
+            {
+                type: 'input',
+                message: 'Who can contribute to this project?',
+                name: 'contribute',
+            },
+            {
+                type: 'input',
+                message: 'What tests does this project use?',
+                name: 'test',
+            },
+            {
+                type: 'input',
+                message: 'What is your Github URL?',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'email',
+            }
+        ]);
         }
-    ]);
-};
 
+    // TODO: Create a function to write README file
+    questions()
+        .then((answers) => writeFileAsync('README.md', generateMarkdown(answers)))
+        .then(() => console.log("Successfully wrote README"))
+        .catch((err) => console.error(err))
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, answers) {
-
-
-// };
-
-
-
-
-
-// TODO: Create a function to initialize app
-const init = async () => {
-    console.log('work');
-    try {
-        const answers = await questions();
-
-        const markdown = generateMarkdown(answers);
-
-        await writeFileAsync('README.md', markdown);
-
-        console.log('Successfully wrote to README');
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-
-
+}
 
 // Function call to initialize app
-init();
+init()
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,63 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // // If there is no license, return an empty string
-// const renderLicenseBadge = (license) => {
-//   if (data.license === "MIT") {
-//     data.license = mit; 
-//   }
-//   else if (data.license === "ISC") { 
-//     data.license = isc; 
-//   }
-//   else if (data.license === "Apache 2.0") { 
-//     data.license = apache 
-//   }
-//   else if (data.license === "GNU GPLv3") { 
-//     data.license = GPLv3;  
-//   }
-//   else if (data.license === "BSD") { 
-//     data.license = bsd; 
-//   }
-//   else if (data.license === "None") { 
-//     return "" }
-
-// };
-
 
 
 
 // TODO: Create a function that returns the license link
 // // If there is no license, return an empty string
-// const renderLicenseLink = () => {
-//   if (data.licence === "MIT") { return "(https://opensource.org/licenses/MIT)" };
-//   if (data.license === "ISC") { return "(https://opensource.org/licenses/ISC)" };
-//   if (data.license === "Apache 2.0") { return "(https://opensource.org/licenses/Apache-2.0)" };
-//   if (data.license === "GNU GPLv3") { return "(https://www.gnu.org/licenses/gpl-3.0)" };
-//   if (data.licence === "BSD") { return "(https://opensource.org/licenses/BSD-3-Clause)" };
-//   if (data.license === "None") { return "" };
-// };
 
-// renderLicenseLink(data.license);
+
 
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
-// const renderLicenseSection = (license) => {
-
-// }
-
-// renderLicenseSection;
-
-// const liscBadge = renderLicenseBadge(data.license);
-// const liscLink = renderLicenseLink(data.license);
-// const liscSection = renderLicenseSection(data.license);
 
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(answers) {
   return `
-  # ${data.title}
+  # ${answer.title}
 
 
 ## Description
-​${data.description} 
+​${answer.description} 
 
 ## Table of Contents 
 ​
@@ -77,7 +39,7 @@ function generateMarkdown(data) {
 ​
 To install necessary dependencies, run the following command:
 ​
-${data.install}
+${answer.install}
 
 ## Usage
 ​
@@ -85,24 +47,24 @@ Use git clone to pull down the repository.
 ​
 ## License
 ​
-${data.license}
+${answer.license}
   
 ## Contributing
 ​
-${data.contribute} can contribute to this repository. 
+${answer.contribute} can contribute to this repository. 
 ​
 ## Tests
 ​
-This project uses the ${data.test} test. 
+This project uses ${answer.test} tests. 
 ​
 ## Questions
 ​
-If you have any questions, please contact me directly at <${data.email}>.
-More of my work can be found at [rmmngtn](https://github.com/${data.github}).
+If you have any questions, please contact me directly at <${answer.email}>.
+More of my work can be found at [rmmngtn](https://github.com/${answer.github}).
 
   `;
 
 
 };
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
